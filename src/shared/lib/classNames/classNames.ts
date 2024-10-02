7ce0dbb23f1489afea73ec89a8e9c5da2f8ed847
Stanislav L <linnikon@gmail.com>
2024-10-02 12:16:21 +0300

    integrate FSD

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,3 +1,4 @@
+
 type Mods = Record<string, boolean | string>
 
 export function classNames(cls: string, mods: Mods, additional: string[]): string {
@@ -5,8 +6,11 @@ export function classNames(cls: string, mods: Mods, additional: string[]): strin
         cls,
         ...additional,
         ...Object.entries(mods)
-            .filter(([classNames, value]) => Boolean(value))
-            .map(([classNames]) => classNames)
+            .filter(([className, value]) => Boolean(value))
+            .map(([className]) => className)
     ]
-        .join(' ')
+        .join(' ');
 }
+
+
+
