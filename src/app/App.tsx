33b8a65dd80b3e27feeacb2f06ed6085193dac7e
Stanislav L <linnikon@gmail.com>
2024-10-10 14:17:40 +0300

    add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
-import './styles/index.scss';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/NavBar';
 import { Sidebar } from 'widgets/Sidebar';
+import './styles/index.scss';
+
 
 
 const App = () => {
@@ -12,11 +14,13 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
